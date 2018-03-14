945bb62bc1b3400ecb1e954ed02d3b39f118bd16
vangheem <vangheem@gmail.com>
2018-03-14 17:03:29 -0400

    be able to authenticate inside cluster

--- aioclustermanager/k8s/__init__.py
+++ aioclustermanager/k8s/__init__.py
@@ -9,50 +9,62 @@ import tempfile
 
 logger = logging.getLogger('aioclustermanager')
 
+SERVICE_HOST_ENV_NAME = "KUBERNETES_SERVICE_HOST"
+SERVICE_PORT_ENV_NAME = "KUBERNETES_SERVICE_PORT"
+SERVICE_TOKEN_FILENAME = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+SERVICE_CERT_FILENAME = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+
+
+def _join_host_port(host, port):
+    """Pulled from kubernetes python package"""
+    template = "%s:%s"
+    host_requires_bracketing = ':' in host or '%' in host
+    if host_requires_bracketing:
+        template = "[%s]:%s"
+    return template % (host, port)
+
 
 class Configuration:
     file = None
+    ssl_context = None
+    cert_file = None
 
     def __init__(self, environment):
+        self.headers = {}
         self.ssl_context = None
         self.environment = environment
-        if environment['certificate'] is not None:
-            # Certificate management
-            ssl_client_context = ssl.create_default_context(
-                purpose=ssl.Purpose.CLIENT_AUTH)
-            self.cert_file = tempfile.NamedTemporaryFile(delete=False)
-            self.cert_file.write(b64decode(environment['certificate']))
-            self.cert_file.close()
-            self.client_key = tempfile.NamedTemporaryFile(delete=False)
-            self.client_key.write(b64decode(environment['key']))
-            self.client_key.close()
 
-            ssl_client_context.load_cert_chain(
-                certfile=self.cert_file.name, keyfile=self.client_key.name)
-            conn = aiohttp.TCPConnector(ssl_context=ssl_client_context)
-            self.session = aiohttp.ClientSession(connector=conn)
-        elif environment['certificate_file'] is not None:
-            logger.debug('Loading cert files')
-            ssl_client_context = ssl.create_default_context(
-                purpose=ssl.Purpose.CLIENT_AUTH)
-            ssl_client_context.load_cert_chain(
-                certfile=environment['certificate_file'],
-                keyfile=environment['key_file'])
-            conn = aiohttp.TCPConnector(ssl_context=ssl_client_context)
-            self.session = aiohttp.ClientSession(connector=conn)
-        else:
-            basic_auth = aiohttp.BasicAuth(
-                environment['user'], environment['credentials'])
-            self.session = aiohttp.ClientSession(auth=basic_auth)
+        if environment.get('in_cluster'):
+            with open(SERVICE_TOKEN_FILENAME) as fi:
+                token = fi.read()
+            self.headers = {
+                'Authorization': 'Bearer ' + token
+            }
+            environment.update({
+                'skip_ssl': True,
+                'endpoint': _join_host_port(
+                    os.environ[SERVICE_HOST_ENV_NAME],
+                    os.environ[SERVICE_PORT_ENV_NAME])
+            })
+
+        if environment.get('certificate') is not None:
+            # Certificate management
+            self.load_certificate()
+        elif environment.get('certificate_file') is not None:
+            self.load_certificate_file()
+        elif environment.get('user') and environment.get('credentials'):
+            self.load_basic_auth()
+        elif environment.get('skip_ssl'):
+            self.load_skip_ssl()
 
         ssl_context = None
-        if environment['ca'] is not None:
+        if environment.get('ca') is not None:
             self.file = tempfile.NamedTemporaryFile(delete=False)
             self.file.write(bytes(environment['ca'], encoding='utf-8'))
             self.file.close()
             self.ssl_context = ssl.SSLContext()
             ssl_context.load_verify_locations(self.file.name)
-        elif environment['ca_file'] is not None:
+        elif environment.get('ca_file') is not None:
             self.ssl_context = ssl.SSLContext()
             self.ssl_context.load_verify_locations(environment['ca_file'])
 
@@ -60,6 +72,48 @@ class Configuration:
             self.verify = False
         else:
             self.verify = True
+
+    def load_skip_ssl(self):
+        self.session = aiohttp.ClientSession(
+            connector=aiohttp.TCPConnector(verify_ssl=False),
+            headers=self.headers)
+
+    def load_basic_auth(self):
+        basic_auth = aiohttp.BasicAuth(
+            self.environment['user'], self.environment['credentials'])
+        self.session = aiohttp.ClientSession(
+            auth=basic_auth, headers=self.headers)
+
+    def load_certificate_file(self):
+        logger.debug('Loading cert files')
+        ssl_client_context = ssl.create_default_context(
+            purpose=ssl.Purpose.CLIENT_AUTH)
+        if 'key_file' in self.environment:
+            ssl_client_context.load_cert_chain(
+                certfile=self.environment['certificate_file'],
+                keyfile=self.environment['key_file'])
+        else:
+            ssl_client_context.load_cert_chain(
+                certfile=self.environment['certificate_file'])
+        conn = aiohttp.TCPConnector(ssl_context=ssl_client_context)
+        self.session = aiohttp.ClientSession(
+            connector=conn, headers=self.headers)
+
+    def load_certificate(self):
+        ssl_client_context = ssl.create_default_context(
+            purpose=ssl.Purpose.CLIENT_AUTH)
+        self.cert_file = tempfile.NamedTemporaryFile(delete=False)
+        self.cert_file.write(b64decode(self.environment['certificate']))
+        self.cert_file.close()
+        self.client_key = tempfile.NamedTemporaryFile(delete=False)
+        self.client_key.write(b64decode(self.environment['key']))
+        self.client_key.close()
+
+        ssl_client_context.load_cert_chain(
+            certfile=self.cert_file.name, keyfile=self.client_key.name)
+        conn = aiohttp.TCPConnector(ssl_context=ssl_client_context)
+        self.session = aiohttp.ClientSession(
+            connector=conn, headers=self.headers)
 
 
 class K8SContextManager:
